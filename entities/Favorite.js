402fa7d3bd5c0d95737ff4e9d202ccd2b8b58d04
Dagim Abyot <dagim045@gmail.com>
2026-03-20 14:34:34 +0300

    Update CineVerse code for GitHub

--- entities/Favorite.js
+++ entities/Favorite.js
@@ -1,1 +1,32 @@
-// Favorite.js - placeholder file
+{
+  "name": "Favorite",
+  "type": "object",
+  "properties": {
+    "movie_id": {
+      "type": "number",
+      "description": "TMDB movie ID"
+    },
+    "title": {
+      "type": "string"
+    },
+    "poster_path": {
+      "type": "string"
+    },
+    "release_date": {
+      "type": "string"
+    },
+    "vote_average": {
+      "type": "number"
+    },
+    "genre_ids": {
+      "type": "array",
+      "items": {
+        "type": "number"
+      }
+    }
+  },
+  "required": [
+    "movie_id",
+    "title"
+  ]
+}
